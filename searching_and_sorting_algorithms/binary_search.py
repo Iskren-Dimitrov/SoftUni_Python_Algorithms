9af86030e4950893565fd9414f67548dbfb06301
Iskren Dimitrov <114501449+Iskren-Dimitrov@users.noreply.github.com>
2023-10-05 17:14:56 +0300

    Problem Solutions for Exercise: searching_and_sorting_algorithms
    add_tasks

---
+++ searching_and_sorting_algorithms/binary_search.py
@@ -0,0 +1,24 @@
+'''Implement an algorithm that finds the index of an element in a sorted array of integers in logarithmic time.'''
+
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid_idx = (left + right) // 2
+        mid_el = nums[mid_idx]
+
+        if mid_el == target:
+            return mid_idx
+
+        if target > mid_el:
+            left = mid_idx + 1
+
+        else:
+            right = mid_idx - 1
+
+
+nums = [int(x) for x in input().split()]
+target = int(input())
+print(binary_search(nums, target))
